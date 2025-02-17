2027deac16b91ad72b932a2e85017e6310257f76
cwilson-su <125030769+cwilson-su@users.noreply.github.com>
2025-02-17 23:06:02 +0100

    Add files via upload

---
+++ tests/custom_agents/test_MedianAgent.py
@@ -0,0 +1,14 @@
+import sys
+import os
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "../..", "src")))
+from customAgents.MedianAgent import MedianAgent
+
+
+if __name__ == "__main__":
+    agent = MedianAgent(track=None, render_mode="human", plot=False) #by default, path_lookahead is 3
+    
+    # agent = MedianAgent(track=None, render_mode="human", path_lookahead=2) # min path_lookahead is 2
+    
+    # Ensure the agent runs by consuming the generator
+    for obs in agent.run():
+        pass  
